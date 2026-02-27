d59bd71c178c44f2a3593dac44d1d09f0b579f7c
Mikołaj Zinkiewicz <zinmiki10@gmail.com>
2026-02-27 21:13:12 +0100

    a

--- src/js/koszyk.js
+++ src/js/koszyk.js
@@ -7,6 +7,8 @@ function addToKoszyk(produkt_id) {
 
     // Save data to localStorage
     localStorage.setItem('koszyk', JSON.stringify(cart));
+
+    renderujKoszyk();
 };
 
 function removeFromKoszyk(produkt_id) {
@@ -15,6 +17,8 @@ function removeFromKoszyk(produkt_id) {
     cart = cart.filter(id => id !== produkt_id);
 
     localStorage.setItem('koszyk', JSON.stringify(cart));
+
+    renderujKoszyk();
 }
 
 function getKoszyk() {
@@ -27,4 +31,29 @@ function clearKoszyk() {
     if (user) {
         localStorage.clear();
     }
+
+    renderujKoszyk();
+}
+
+function renderujKoszyk() {
+    const products = document.getElementById('products');
+    const cart = getKoszyk();
+
+    // clear elements!!
+    products.innerHTML = "";
+
+    cart.forEach(id => {
+        const kafelek = `
+            <div class="produkt_kafelek" data-id="${id}">
+                <img src="img/kur.jpg" class="img">
+                <span class="category">Moda</span>
+                <span class="title">Produkt ID: ${id}</span>
+                <span class="price">0.05 zł</span>
+                <a href="#" class="buy del" onclick="removeFromKoszyk(${id})">Usuń</a>
+            </div>
+        `;
+        
+        // Dodajemy kafelek do strony
+        products.innerHTML += kafelek;
+    });
 }
